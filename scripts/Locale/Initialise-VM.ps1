c2531b935d28e52317195c8dba87932bbbebc854
Jeff Field <jeff.field@clouddirect.net>
2018-08-22 16:25:51 +0100

    Updated Data Disk Naming

--- scripts/Locale/Initialise-VM.ps1
+++ scripts/Locale/Initialise-VM.ps1
@@ -7,11 +7,12 @@
 # Set languages/culture
 Set-Culture en-GB
 
-# Add/format any raw data disks and name them as per their LUN ID.
+# Add/format any raw data disks and name them.
 $CandidateRawDisks = Get-Disk |  Where {$_.PartitionStyle -eq 'raw'} | Sort -Property Number
 Foreach ($RawDisk in $CandidateRawDisks) {
     $LUN = (Get-WmiObject Win32_DiskDrive | Where index -eq $RawDisk.Number | Select SCSILogicalUnit -ExpandProperty SCSILogicalUnit)
-    $Disk = Initialize-Disk -PartitionStyle MBR -Number $RawDisk.Number
+    $Disk = Initialize-Disk -PartitionStyle GPT -Number $RawDisk.Number
     $Partition = New-Partition -DiskNumber $RawDisk.Number -UseMaximumSize -AssignDriveLetter
-    $Volume = Format-Volume -Partition $Partition -FileSystem NTFS -NewFileSystemLabel "DATA-$LUN" -Confirm:$false
+    $Label = $LUN +1
+    $Volume = Format-Volume -Partition $Partition -FileSystem NTFS -NewFileSystemLabel "Data-$Label" -Confirm:$false
 }
